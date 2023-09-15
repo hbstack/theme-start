3210db53a3c151beba12238ab68a02fe6f3f9d72
razonyang <razonyang@gmail.com>
2023-09-15 18:01:03 +0800

    chore: fix lint issues

--- assets/hb/modules/theme-start/js/form.ts
+++ assets/hb/modules/theme-start/js/form.ts
@@ -4,19 +4,19 @@
   const engines = document.querySelectorAll<HTMLElement>('.hb-start-search-engine')
   const toggle = document.querySelector<HTMLElement>('.hb-start-search-engine-toggle')
 
-  if (!form || !engines) {
+  if (form === null || engines === null) {
     return
   }
 
   input?.focus()
 
-  const activeEngine = (engine: HTMLElement) => {
+  const activeEngine = (engine: HTMLElement): void => {
     engines.forEach((el) => {
       el.classList.remove('active')
     })
     engine.classList.add('active')
-    form.setAttribute('action', engine.getAttribute('data-url')??'')
-    input?.setAttribute('name', engine.getAttribute('data-input-name')??'q')
+    form.setAttribute('action', engine.getAttribute('data-url') ?? '')
+    input?.setAttribute('name', engine.getAttribute('data-input-name') ?? 'q')
     toggle.innerHTML = engine.querySelector('svg')?.outerHTML
   }
 
--- assets/hb/modules/theme-start/js/taxonomies.ts
+++ assets/hb/modules/theme-start/js/taxonomies.ts
@@ -1,36 +1,35 @@
 (() => {
-  const apps = document.querySelectorAll(".hb-start-app");
+  const apps = document.querySelectorAll('.hb-start-app')
   const taxonomies = document.querySelectorAll<HTMLElement>(
-    ".hb-start-app-taxonomy",
-  );
-  if (!taxonomies) {
-    return;
+    '.hb-start-app-taxonomy'
+  )
+  if (taxonomies === null) {
+    return
   }
-  const active = (item: HTMLElement) => {
+  const active = (item: HTMLElement): void => {
     taxonomies.forEach((el) => {
       // change the state of tabs
       if (el !== item) {
-        el.classList.remove("active");
-        el.setAttribute("aria-selected", "false");
+        el.classList.remove('active')
+        el.setAttribute('aria-selected', 'false')
       }
-      item.classList.add("active");
-      item.setAttribute("aria-selected", "true");
+      item.classList.add('active')
+      item.setAttribute('aria-selected', 'true')
       // show and hide apps
-      const parent = item.getAttribute("data-id");
-      console.log(parent);
+      const parent = item.getAttribute('data-id')
       apps.forEach((app) => {
-        if (parent && app.getAttribute("data-parent") !== parent) {
-          app.classList.add("inactive");
+        if (parent !== '' && app.getAttribute('data-parent') !== parent) {
+          app.classList.add('inactive')
         } else {
-          app.classList.remove("inactive");
+          app.classList.remove('inactive')
         }
-      });
-    });
-  };
+      })
+    })
+  }
 
   taxonomies.forEach((el) => {
-    el.addEventListener("click", (e) => {
-      active(el);
-    });
-  });
-})();
+    el.addEventListener('click', (e) => {
+      active(el)
+    })
+  })
+})()
